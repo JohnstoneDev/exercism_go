bde43eeda3a7d7e264c4e4d65dabedd8c9adbd15
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-07-28 11:43:25 +0300

    feat: Plain For Loop; collatz_conjencture

---
+++ collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,22 @@
+package collatzconjecture
+
+import "errors"
+
+func CollatzConjecture(n int) (int, error) {
+	count := 0
+
+	if n < 0 {
+		return count, errors.New("number is negative")
+	}
+
+	for n > 1 {
+		divisible := n % 2 == 0 // check divisibilty of the number
+		if divisible {
+			n /= 2
+		} else {
+			n = 3 * n +1
+		}
+		count++
+	}
+	return count, nil
+}
